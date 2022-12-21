b5a0f732da0a5aeea7c96a14c8b6be4594bd0e6d
Abdelrhman Ahmed <49564335+MrBlackvanta@users.noreply.github.com>
2022-12-21 15:57:55 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+import { Fragment } from "react";
+import NavBar from "./components/NavBar";
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <Fragment>
+      <NavBar />
+    </Fragment>
+  );
+}
+
+export default App;
+
+
 
