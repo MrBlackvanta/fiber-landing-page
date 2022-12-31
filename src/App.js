854adf4bd35448849ac37357b695c3534a6bde4f
Abdelrhman Ahmed <49564335+MrBlackvanta@users.noreply.github.com>
2022-12-31 13:52:13 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,20 @@
+import { Fragment } from "react";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <Fragment>
+      <div className={styles["heading-section"]}>
+        <NavBar />
+        <Hero />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
+
+
 
