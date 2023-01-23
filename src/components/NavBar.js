4cfc6e21d1d6ad3eb6368f67a3d642bc13786ac0
Abdelrhman Ahmed <49564335+MrBlackvanta@users.noreply.github.com>
2023-01-23 14:11:07 +0200

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,1 +1,36 @@
+import styles from "./NavBar.module.scss";
 
+const NavBar = () => {
+  const links = [
+    { id: Math.random(), title: "Community", target: "#" },
+    { id: Math.random(), title: "Pricing", target: "#" },
+    { id: Math.random(), title: "Features", target: "#" },
+  ];
+
+  return (
+    <nav className={styles["nav"]}>
+      <h2 className={styles["nav-title"]}>Fiber</h2>
+
+      <ul className={styles["nav-links"]}>
+        {/* manage portals */}
+        {links.map((link) => (
+          <li key={link.id} className={styles["nav-links__link"]}>
+            <a href={link.target}>{link.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className={styles["nav-buttons"]}>
+        {/* manage portals */}
+        <button className={styles["nav-buttons__button"]}>Sign In</button>
+        <button
+          className={`${styles["nav-buttons__button"]} ${styles["nav-buttons__button-sign-up"]}`}
+        >
+          Sign Up
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
